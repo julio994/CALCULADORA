dc06b0ab234da4f8e02a9b0b79e35e12650f9b9f
“julio994” <julio994@utp.edu.co>
2024-04-05 22:57:57 -0500

    correccion de incidentes

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,11 @@
-def Suma(numero1,numero2 ):
-    suma=numero1+numero2
-    return suma
+def suma(numero1,numero2 ):
+    try:
+        numero1 = int(numero1)
+        numero2 = int(numero2)
+    except:
+        print("debes ingresar solamente numeros")
+        return None
+    else:
+        suma=numero1+numero2
+        return suma
 
