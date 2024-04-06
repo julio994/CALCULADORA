f29450d6d1993070620162314d2020165e01c2e9
Luis Hernandez <luishernandez@utp.edu.co>
2024-04-05 22:22:38 -0500

    agrego menu principal

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Calculadora")
+from calculadora import suma
+
+continuar = True
+
+while(continuar):
+    print("Calculadora")
+    print("Elija la opción deseada:")
+    print("1. Suma")
+    print("2. Salir")
+
+    opc = input("")
+    if(opc == "1"):
+        numeroUno = int(input("ingrese el primer numero: "))
+        numeroDos = int(input("ingrese el segundo numero: "))
+        resultado = suma(numeroDos,numeroDos)
+        print(f"{numeroUno}+{numeroDos}={resultado}")
+    elif(opc == "2"):
+        print("Calculadora cerrada.")
+        continuar = False
+    else:
+        print("opción incorrecta")
